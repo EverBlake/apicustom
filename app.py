5b81c4620542bb6baf9aa117e789775b81254adf
Blake Everett <70494174+EverBlake@users.noreply.github.com>
2023-06-23 18:40:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,34 @@ def homepage():
 @app.route('/hello', methods=['GET'])
 def hello():
     return "Hello"
+
+# Set up your OpenAI API credentials
+openai.api_key = process.env.OPENAI_API_KEY
+
+# API endpoint for generating a marketing report
+@app.route('/generate-marketing-report', methods=['POST'])
+def generate_marketing_report():
+    # Get the URL from the request body
+    url = request.json['url']
+
+    # Define the prompt for the marketing report
+    prompt = f"Marketing Report for {url}:\n"
+
+    # Generate the report using the OpenAI API
+    response = openai.Completion.create(
+        engine='gpt-3.5-turbo-16k',  # Use the new 16k token model
+        prompt=prompt,
+        max_tokens=15999,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+
+    # Get the generated report text from the API response
+    report = response.choices[0].text.strip()
+
+    # Return the report as a JSON response
+    return jsonify({'report': report})
+
+if __name__ == '__main__':
+    app.run()
